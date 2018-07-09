5d63beae4f20ff245bc7bcd4b7f247d4fc2ae6c5
Dale Newman <dalenewman@gmail.com>
2018-07-09 11:17:38 -0400

    Fixed nuget dependency

--- nuget/nuget.bat
+++ nuget/nuget.bat
@@ -1,8 +1,8 @@
 nuget pack Transformalize.Provider.SqlServer.nuspec -OutputDirectory "c:\temp\modules"
 nuget pack Transformalize.Provider.SqlServer.Autofac.nuspec -OutputDirectory "c:\temp\modules"
 
-nuget push "c:\temp\modules\Transformalize.Provider.SqlServer.0.3.6-beta.nupkg" -source https://api.nuget.org/v3/index.json
-nuget push "c:\temp\modules\Transformalize.Provider.SqlServer.Autofac.0.3.6-beta.nupkg" -source https://api.nuget.org/v3/index.json
+nuget push "c:\temp\modules\Transformalize.Provider.SqlServer.0.3.7-beta.nupkg" -source https://api.nuget.org/v3/index.json
+nuget push "c:\temp\modules\Transformalize.Provider.SqlServer.Autofac.0.3.7-beta.nupkg" -source https://api.nuget.org/v3/index.json
 
 
 
