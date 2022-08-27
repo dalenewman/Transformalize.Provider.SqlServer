10cc9d02347a78e4b05c5dbc9702f2923a431ad5
Dale Newman <dalenewman@gmail.com>
2022-08-27 12:59:26 -0400

    bump dependencies to make sure hashcode is handled properly in tranformalize

--- nuget/myget.bat
+++ nuget/myget.bat
@@ -1,2 +1,2 @@
-nuget push "c:\temp\modules\Transformalize.Provider.SqlServer.0.10.7-beta.nupkg" -source https://www.myget.org/F/transformalize/api/v3/index.json
-nuget push "c:\temp\modules\Transformalize.Provider.SqlServer.Autofac.0.10.7-beta.nupkg" -source https://www.myget.org/F/transformalize/api/v3/index.json
+nuget push "c:\temp\modules\Transformalize.Provider.SqlServer.0.10.9-beta.nupkg" -source https://www.myget.org/F/transformalize/api/v3/index.json
+nuget push "c:\temp\modules\Transformalize.Provider.SqlServer.Autofac.0.10.9-beta.nupkg" -source https://www.myget.org/F/transformalize/api/v3/index.json
