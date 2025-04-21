82738f3413421eea1bd5c9d7af7a27fcd29bd578
Dale Newman <dalenewman@gmail.com>
2025-04-20 23:16:59 -0400

    update ado dependency

--- nuget/nuget.bat
+++ nuget/nuget.bat
@@ -1,5 +1,5 @@
 REM nuget pack Transformalize.Provider.SqlServer.nuspec -OutputDirectory "c:\temp\modules"
 REM nuget pack Transformalize.Provider.SqlServer.Autofac.nuspec -OutputDirectory "c:\temp\modules"
 
-REM nuget push "c:\temp\modules\Transformalize.Provider.SqlServer.0.11.1-beta.nupkg" -source https://api.nuget.org/v3/index.json
-REM nuget push "c:\temp\modules\Transformalize.Provider.SqlServer.Autofac.0.11.1-beta.nupkg" -source https://api.nuget.org/v3/index.json
+REM nuget push "c:\temp\modules\Transformalize.Provider.SqlServer.0.11.2-beta.nupkg" -source https://api.nuget.org/v3/index.json
+REM nuget push "c:\temp\modules\Transformalize.Provider.SqlServer.Autofac.0.11.2-beta.nupkg" -source https://api.nuget.org/v3/index.json
